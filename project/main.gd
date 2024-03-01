2ffb5916af78c62be82b80005c8bc26b3656aa87
Hayden Leete <haydenleete@gmail.com>
2024-03-02 11:16:25 +1300

    update demo

--- project/main.gd
+++ project/main.gd
@@ -1,17 +1,30 @@
 extends Node2D
 
-@onready var markers : Array[Node2D] = [$Cursor1, $Cursor2, $Cursor3]
+var cursors : Array[Polygon2D] = []
 
 func _ready() -> void:
-	Input.set_mouse_mode(Input.MOUSE_MODE_CONFINED)
+	# For convenience sake. Raw mouse input only works when the window is focused,
+	# So we want to keep the real mouse in the window to stop it from clicking elsewhere
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+	for i in RawInput.get_mouse_device_count():
+		var cursor = $Cursor.duplicate() as Polygon2D
+		cursor.color = Color.from_hsv(i * 0.2, 0.9, 1.0)
+		cursor.position += Vector2(64, 0).rotated(i * 0.2)
+		RawInput.warp_mouse_position(cursor.position, i)
+		add_child(cursor)
+		cursors.append(cursor)
+
 	RawInput.input_event.connect(_on_raw_input_input_event)
 
 func _process(delta: float) -> void:
-	for i in 2:
-		markers[i].position = RawInput.get_mouse_position(i)
+	for i in cursors.size():
+		var cursor = cursors[i]
+		cursor.position = RawInput.get_mouse_position(i)
 		if RawInput.is_mouse_button_pressed(MOUSE_BUTTON_LEFT, i):
-			markers[2].position = markers[2].position.move_toward(markers[i].position, 300*delta)
+			$Cursor.position = $Cursor.position.move_toward(cursor.position, 300*delta)
 
+# recenter on middle mouse button
 func _on_raw_input_input_event(event: InputEvent) -> void:
 	if event is InputEventMouseButton:
 		if event.pressed and event.button_index == MOUSE_BUTTON_MIDDLE:
